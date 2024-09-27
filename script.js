7206e20c0238aabbba69c5f18b5613cf33f0aee1
amandaversiani <amandaversianni@gmail.com>
2024-09-26 21:49:04 -0300

    criei index.html

---
+++ script.js
@@ -0,0 +1,15 @@
+var nomes = [];
+var idades = [];
+var linguagens = [];
+
+function guardarDados(){
+    var nome = document.getElementById("nome").value;
+    var idade = document.getElementById("idade").value;
+    var linguagens = document.getElementById("linguagem").
+    options[document.getElementById("linguagem").
+        selectedIndex].innerHTML;
+
+    nome.push(nome);
+    idades.push(idade);
+    linguagens.push(linguagem);
+}
